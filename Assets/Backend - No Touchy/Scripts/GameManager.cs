81734bbbdc454d300c5a72a064d30e56b9c871a5
Batzuga <jere.ahvenus@gmail.com>
2025-10-07 14:00:38 +0300

    Mission 8 - Quiz Master returns

--- Assets/Backend - No Touchy/Scripts/GameManager.cs
+++ Assets/Backend - No Touchy/Scripts/GameManager.cs
@@ -17,8 +17,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject bubble;
     [SerializeField] TextMeshPro bubbleTxt;
     Vector2 startP;
-    [SerializeField] Lock[] locks;
-    [SerializeField] Key[] keys;
 
     private void Awake()
     {
@@ -36,31 +34,8 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         player = GameObject.FindFirstObjectByType<Player>();
         startP = player.transform.position;
-        KeyTest();
         
     }
-
-    void KeyTest()
-    {
-        List<GameObject> hits = new List<GameObject>();
-        foreach (Key key in keys)
-        {
-            Vector2 pos = key.transform.position;
-            pos.y = 6.5f;
-            float dist = Vector2.Distance(key.transform.position, pos);
-            if (dist > 0.1f)
-            {
-                hits.Add(key.gameObject);
-            }
-        }
-        for(int i = 0; i < hits.Count; i++)
-        {
-            Destroy(hits[i]);
-            bubbleTxt.text = "I wanted that trophy, but not like this... Not like this...";
-            Debug.LogError($"Key with id: " + hits[i].GetComponent<Key>().keyID + " destroyed due to foul play. Shame on you. Set the key Y position to 6.5f.");
-        }
-    }
-
     public void HideBubble()
     {
         bubble.SetActive(false);
@@ -70,26 +45,7 @@ public class GameManager : MonoBehaviour
         if (Vector2.Distance(player.transform.position, startP) > 0.2f)
         {
             bubble.SetActive(false);
-        }
-        try
-        {
-            bool fin = true;
-            if(locks.Length == 0) fin = false;
-            foreach(Lock l in locks)
-            {
-                if(l.opened == false)
-                {
-                    fin = false;
-                }
-            }
-            Trophy.instance.Toggle(fin);
-        }
-        catch(Exception e)
-        {
-            Trophy.instance.Toggle(false);
-        }
-       
-        
+        }     
     }
 
     public bool MissionComplete()
